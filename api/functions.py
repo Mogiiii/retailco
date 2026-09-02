ca62d885b7ad488674590c7df2ec0e928e826f39
Dylan Nantz <Nantz.Dylan@gmail.com>
2026-09-02 17:58:22 -0400

    add basic api functionality

--- api/functions.py
+++ api/functions.py
@@ -1,4 +1,38 @@
-def handler(event, context):
-    return {
-        "statusCode": 200
-    }
+import json
+import os
+import uuid
+from decimal import Decimal
+
+import boto3
+from pydantic import BaseModel
+from types_boto3_dynamodb.service_resource import Table
+
+
+class taxrateentry(BaseModel):
+    id: int
+    category: int
+    taxrate: Decimal
+
+
+dynamodb = boto3.resource("dynamodb")
+table: Table = dynamodb.Table("retailco-taxrates")
+
+
+def Ok(body):
+    return {"statusCode": 200, "body": body}
+
+
+def get_all(event, context):
+    #TODO handle pagination
+    result = table.scan()
+    return Ok(result.get("Items", []))
+
+
+# upsert => create or update
+def upsert(event, context):
+    to_add: list[taxrateentry] = json.loads(event["body"])
+    with table.batch_writer() as br:
+        for entry in to_add:
+            i = {"Category": entry.category, "taxrate": entry.taxrate}
+            _ = br.put_item(Item=i)
+    return  Ok("OK")
