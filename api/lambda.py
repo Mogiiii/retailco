e60c51fcf846a998f7df28de509286da1d716605
Dylan Nantz <Nantz.Dylan@gmail.com>
2026-09-06 22:46:30 -0400

    update document processing code

--- api/lambda.py
+++ api/lambda.py
@@ -83,6 +83,7 @@ def request_upload_url(event, context):
         "status": "awaiting_upload",
         "s3_location": upload_location,
         "createdAt": datetime.now(),
+        "content_type": content_type,
     }
     tax_document_table.put_item(Item=i)
 
@@ -103,58 +104,65 @@ def request_upload_url(event, context):
         ),
     }
 
+
 def process_document(event, context):
     s3_client = boto3.client("s3")
 
-    record = event["Records"]
+    record = event["Records"][0]
     bucket = record["s3"]["bucket"]["name"]
     key = record["s3"]["object"]["key"]
     id = key.split("/")[1]
 
     response = s3_client.get_object(Bucket=bucket, Key=key)
 
-    file_content = response["Body"].read()
+    file_data = response["Body"].read()
 
     print("bucket:" + bucket)
     print("key:" + key)
     print("id:" + id)
-    print("file content:" + file_content)
 
     table_data = tax_document_table.get_item(Key={"id": id})
+    content_type = table_data["Item"]["content_type"]
     print(json.dumps(table_data))
-    
-    # openapi_api_key = os.environ["OPENAI_API_KEY"]
-    # client = OpenAI(api_key=openapi_api_key)
 
-    # # TODO pagination
-    # tax_categories = tax_rates_table.scan()
-    # try:
-    #     response = client.responses.parse(
-    #         model="gpt-4o-mini",
-    #         input=[
-    #             {
-    #                 "role": "user",
-    #                 "content": [
-    #                     {
-    #                         "type": "input_file",
-    #                         "filename": "document",
-    #                         "file_data": file_content,
-    #                     },
-    #                     {
-    #                         "type": "input_text",
-    #                         "text": "Analyze the file and assign tax categories according to the following data:\n"
-    #                         + json.dumps(tax_categories),
-    #                     },
-    #                 ],
-    #             }
-    #         ],
-    #         text_format=tax_summary,
-    #     )
+    openapi_api_key = os.environ["OPENAI_API_KEY"]
+    client = OpenAI(api_key=openapi_api_key)
 
-    #     i = {"id": id, "status": "complete", "data": response}
-    #     tax_document_table.put_item(Item=i)
+    file_content = f"data:{content_type};base64,{file_data}"
+    # TODO pagination
+    tax_categories = tax_rates_table.scan()
+    try:
+        response = client.responses.parse(
+            model="gpt-4o-mini",
+            input=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_file",
+                            "filename": "document",
+                            "file_data": file_content,
+                        },
+                        {
+                            "type": "input_text",
+                            "text": "Analyze the file and assign tax categories according to the following data:\n"
+                            + json.dumps(tax_categories),
+                        },
+                    ],
+                }
+            ],
+            text_format=tax_summary,
+        )
 
-    # except Exception as e:
-    #     i = {"id": id, "status": "complete", "data": [], "error": e}
-    #     tax_document_table.put_item(Item=i)
-    #     print(e)
+        i = {
+            "id": id,
+            "status": "complete",
+            "data": response,
+            "createdAt": table_data["Item"]["createdAt"],
+        }
+        tax_document_table.put_item(Item=i)
+
+    except Exception as e:
+        i = {"id": id, "status": "failed", "data": [], "error": e}
+        tax_document_table.put_item(Item=i)
+        print(e)
